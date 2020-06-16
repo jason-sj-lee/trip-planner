e0ed8b420cbc802f4e54f7b654f7320c1740ada2
Jason <jason.981214@gmail.com>
2020-06-16 15:08:29 -0400

    added routing capability

--- trip-planner/src/App.vue
+++ trip-planner/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>App</h1>
+    <router-view></router-view>
+    <router-link to="/">Home</router-link>
+    <router-link to="/results">Results</router-link>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ trip-planner/src/components/Results.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Results</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Results',
+  props: {
+  }
+}
+</script>
+
+<style>
+</style>
