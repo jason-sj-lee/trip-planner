a413273654c719cdec92de59ee6d991b7a039b16
Andrew Ma <hyung.joon2231621@gmail.com>
2020-06-16 19:25:58 -0400

    removed default app styling and tag in App.vue

--- trip-planner/src/App.vue
+++ trip-planner/src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-    <h1>App</h1>
-    <router-view></router-view>
     <nav-bar></nav-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     NavBar
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
