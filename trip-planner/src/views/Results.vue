95bcacabddd1e3ec5a54f8dd091fc28816e8dde2
Andrew Ma <hyung.joon2231621@gmail.com>
2020-06-19 14:51:13 -0400

    results page with cloud bg

--- trip-planner/src/views/Results.vue
+++ trip-planner/src/views/Results.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an results page</h1>
+    <div class="content">
+      <h1>asdf</h1>
+    </div>
+    <v-img class="cloud-bg" src="../assets/cloud-bg.jpg"></v-img>
   </div>
 </template>
+
+<style scoped>
+.cloud-bg {
+  z-index: 0;
+  position: absolute;
+  height: 1000px;
+  width: 100%;
+  bottom: 0;
+}
+
+.content {
+  position: relative;
+  z-index: 0;
+  background-color: blue;
+  height: 1000px;
+}
+</style>
